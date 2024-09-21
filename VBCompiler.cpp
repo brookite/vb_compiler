af0308079a63f9f94ba8d76b2e6eeed22450a545
Dmitry Sh (Brookit) <brookit18@gmail.com>
2024-09-21 10:35:00 +0300

    lexer: Правила для сложных лексем

--- VBCompiler.cpp
+++ VBCompiler.cpp
@@ -1,6 +1,12 @@
 ﻿#include <iostream>
+#include "lexer/lexer.flex.cpp"
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main(int argc, char** argv) {
+    if (argc > 1) {
+        fopen_s(&yyin, argv[1], "r");
+        yylex();
+    }
+    else {
+        printf("File not found");
+    }
 }
