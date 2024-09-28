d6ba5130c350c16681b5e0babd6d4b73ecc220b5
Ekaterina351 <137607084+Ekaterina351@users.noreply.github.com>
2024-09-28 23:43:28 +0300

    lexer:Добавлены разделители и операторы

--- lexer/tests/lexer_comment_test.vb
+++
@@ -1,7 +0,0 @@
-' It's a comment
-REM It's also a comment
-
-' Comment with escaped \n \t \r \"
-' Comment with quotes "abc"
-' REM comment (rem not ignored)
-here not a comment
--- lexer/tests/lexer_string_test.vb
+++
@@ -1,9 +0,0 @@
-"abc"
-"abc"" with quote "" "
-"abc\x56"
-"abc\\r"
-"abc\\r\\n\\t"
-"abc\t2\n"
-"abc\\\\test"
-"abc
-"
